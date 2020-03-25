d8c4ecd20007a9b057e956e1297745e8e95c13f3
Tammivuori <marko.tammivuori@gmail.com>
2020-03-25 13:52:28 +0200

    3.3: puhelinluettelon backend step3

--- index.js
+++ index.js
@@ -9,37 +9,37 @@ let persons = [
       {
         "name": "Marko",
         "number": "98989",
-        "id": 12
+        "id": 1
       },
       {
-        "name": "Pena",
+        "name": "Pena Kukkuu",
         "number": "0909",
-        "id": 15
+        "id": 2
       },
       {
         "name": "Kalle",
         "number": "7767676",
-        "id": 16
+        "id": 3
       },
       {
         "name": "Jaska",
         "number": "88",
-        "id": 17
+        "id": 4
       },
       {
         "name": "Ööö",
         "number": "8989",
-        "id": 18
+        "id": 5
       },
       {
         "name": "Aaa",
         "number": "0900",
-        "id": 19
+        "id": 6
       },
       {
         "name": "Äiti",
         "number": "909090",
-        "id": 21
+        "id": 7
       }
     ]
 
@@ -55,11 +55,22 @@ app.get('/info', function (req,res) {
     res.send(response)
 })
 
-
+// To get all persons
 app.get('/api/persons', (req, res) => {
     res.json(persons)
     })
 
+
+// Person info based on id number
+app.get('/api/persons/:id', function (req,res) {
+    const id = Number(req.params.id)
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        res.json(person)
+    } else {
+        // If person is not found by if return 404
+        res.status(404).end()
+    }})
 
 const port = 3001
 app.listen(port)
