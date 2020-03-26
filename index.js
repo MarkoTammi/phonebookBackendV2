1ed45d161727bf9937dc5e561c73f753fc51403c
Tammivuori <marko.tammivuori@gmail.com>
2020-03-26 10:23:27 +0200

    3.6: puhelinluettelon backend step6

--- index.js
+++ index.js
@@ -5,26 +5,15 @@
 const express = require('express')
 const app = express()
 
+// Express's json parser
+app.use(express.json())
+
+
 let persons = [
       {
         "name": "Marko",
         "number": "98989",
         "id": 1
-      },
-      {
-        "name": "Pena Kukkuu",
-        "number": "0909",
-        "id": 2
-      },
-      {
-        "name": "Kalle",
-        "number": "7767676",
-        "id": 3
-      },
-      {
-        "name": "Jaska",
-        "number": "88",
-        "id": 4
       },
       {
         "name": "Ööö",
@@ -48,20 +37,20 @@ app.get('/', (request, response) => {
     })
 
 
-// Info how many names in phonebook with timestamp
+// Return info how many names in phonebook with timestamp
 app.get('/info', function (request,response) {
     const personLength = persons.length.toString()
     const resContent = '<p>Phonebook has info for '+ personLength + ' people</p><p>' + new Date() + '</p>'
     response.send(resContent)
 })
 
-// To get all persons
+// Return all persons
 app.get('/api/persons', (request, response) => {
     response.json(persons)
     })
 
 
-// Person info based on id number
+// Return person info based on id number
 app.get('/api/persons/:id', function (request,response) {
     const id = Number(request.params.id)
     const person = persons.find(person => person.id === id)
@@ -71,6 +60,41 @@ app.get('/api/persons/:id', function (request,response) {
         // If person is not found by id return 404
         response.status(404).end()
     }})
+
+
+// Add new person
+app.post('/api/persons/', function (request,response) {
+
+    const newId = Math.floor(Math.random() * Math.floor(100000))    
+    const body = request.body 
+
+    // POST request headers
+    //console.log("POST request headers", request.headers)
+
+    // Error if name or number field is empty
+    if (!body.name || !body.number) {
+        return response.status(400).json({error: 'Name or number is missing'})
+    }
+
+    // Error if new name already exist
+    if (persons.find(person => person.name === body.name) ) {
+        return response.status(400).json({error: 'Name already exist. Name have to be unique.'})
+    }
+
+    // create new person object from request data with random id
+    const newPerson = {
+        name : body.name, 
+        number : body.number,
+        id : newId 
+    }
+
+    // Add new person to phonebook
+    persons = persons.concat(newPerson)
+
+    // Return new person with id
+    response.json(newPerson)
+})
+
 
 // Delete person based on id
 app.delete('/api/person/:id', (request, response) => {
@@ -83,6 +107,13 @@ app.delete('/api/person/:id', (request, response) => {
         // If person is not found by id return 404
         response.status(404).end()
     }})
+
+
+// Unknown end poist
+const unknownEndpoint = (req,res) => {
+    res.status(404).send({ error: 'Unknown endpoint'})
+  }
+app.use(unknownEndpoint)
 
 
 const port = 3001
